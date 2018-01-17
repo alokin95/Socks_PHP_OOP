d3bff151f9ca87fc8144faad30fde43b66745927
alokin95 <nikola_m95@hotmail.com>
2018-01-17 03:07:15 +0100

    Added file for products by category query

---
+++ views/products.php
@@ -0,0 +1,28 @@
+<?php
+
+$categoryid = $_GET['category'];
+if (empty($categoryid)){
+  //header("Location:index.php?page=1");
+  $categoryid = 1;
+}
+$get_products = "SELECT * FROM products p JOIN category c ON p.categoryid = c.categoryid JOIN image i ON p.imageid = i.imageid WHERE p.categoryid = ".$categoryid;
+
+$products = mysqli_query($conn, $get_products);
+?>
+
+<section class='products-content flex-row'>
+  <div class='filter'>
+  </div>
+  <div class='products'>
+    <?php
+      
+      while ($product = mysqli_fetch_array($products)){
+        echo 
+        "<div class='product'>
+         <a href='index.php?page=4&productid=".$product['productid']."'><img src='".$product['src']."' alt='".$product['alt']."'></a>
+        </div>";
+      }
+    ?>
+  
+  </div>
+</section>
