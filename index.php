05a62fa58f70945edf8576c5095810b115c230f5
alokin95 <nikola_m95@hotmail.com>
2018-01-17 03:05:31 +0100

    Added single product page

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php 
-
+  
   require_once 'views/header.php';
 
-  if ($_GET['page']==1){
+  if (!isset($_GET['page']) || $_GET['page']==1){
+    $_GET['page']='daa';
     require_once 'views/home.php';
   }
 
   if ($_GET['page']==2){
+    require_once 'views/products.php';
+  }
+
+  if ($_GET['page']==3){
     require_once 'views/register.php';
   }
-  
+
+  if ($_GET['page']==4){
+    require_once 'views/details.php';
+  }
+
   require_once 'views/footer.php';
