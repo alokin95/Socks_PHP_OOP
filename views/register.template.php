76fed0001867c5cae6725f3e15b02ffae6ff2b92
alokin95 <nikola_m95@hotmail.com>
2018-02-07 04:01:22 +0100

    Minor styling

--- views/register.template.php
+++ views/register.template.php
@@ -1,22 +1,21 @@
-<?php
-
-
-
-?>
 
 <section class="register">
   <h1>Create New Customer Account</h1>
   <form id='insert' method='POST' action="/register">
+    <p>First name: </p>
     <input type='text' name='firstName' id='firstName'/><br/>
+    <p>Last name: </p>
     <input type='text' name='lastName' id='lastName'/><br/>
+    <p>Password: </p>
     <input type='password' name='password' id='password'/><br/>
+    <p>E-mail: </p>
     <input type='text' name='email' id='email'/><br/>
-    <input type="submit" id="register" class='register-btn' value='Register' name='register'>
+    <button type="submit" id="register" class='register-btn' name='register'>Register</button>
   </form>
 </section>
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
 
 <script>
 
