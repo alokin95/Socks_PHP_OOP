0835f1a5cbca419350dd9ec89fe78ab9d1d3fa15
alokin95 <nikola_m95@hotmail.com>
2018-02-11 05:50:18 +0100

    Refactoring product class

--- classes/Product.php
+++ classes/Product.php
@@ -2,22 +2,37 @@
 
   class Product extends Connection{
     
-    private $id;
+    private $categoryid;
+    private $productid;
 
     public function __construct() {
 
       parent::__construct();
-      $this->id = $_GET['category'] ?? 1;
+      $this->categoryid = $_GET['category'] ?? 1;
 
     }
 
     public function getProducts() {
 
-      $sql = "SELECT * FROM product p JOIN category c ON p.categoryid = c.categoryid JOIN image i ON p.imageid = i.imageid WHERE p.categoryid = ".$this->id;
+      $sql = "SELECT * FROM product p JOIN category c ON p.categoryid = c.categoryid JOIN image i ON p.imageid = i.imageid WHERE p.categoryid = ".$this->categoryid;
       $result = $this->execute($sql);
       //$result = mysqli_query($this->conn, $sql);
 
       return $result;
+
+    }
+
+    public function getInfo() {
+
+      $this->id = $_GET['productid'] ?? 1;
+
+      $sql = "SELECT productid AS id, p.name as productname, description as productdescription, price as productprice, p.categoryid as catid, genderid as genderid, p.imageid as imgid, src, alt FROM product p JOIN category c ON p.categoryid = c.categoryid JOIN image i ON p.imageid = i.imageid WHERE productid =".$this->id;
+
+      $result = $this->execute($sql);
+
+      $r = mysqli_fetch_object($result);
+
+      return $r;
 
     }
 
