c4bb22fdc9412702ccff4a1bd00f7e17c8e82d96
alokin95 <nikola_m95@hotmail.com>
2018-02-12 01:13:59 +0100

    Added session and productid

--- controllers/comment_add.php
+++ controllers/comment_add.php
@@ -2,21 +2,20 @@
 
   require_once 'classes/Comment.php';
 
-  if (isset($_SESSION['userid'])){
-
-    $productid = $_GET['productid'];
+    
     
     if (isset($_POST['review'])){
-
+      $userid = $_SESSION['userid'];
       $heading = $_POST['heading'];
       $content = $_POST['content'];
+      $productid = $_POST['productid'];
 
       $comm = new Comment;
 
-      $comm->addComment($heading, $content, $productid, $userid);
+      $comm->addComment($heading, $content, $userid, $productid);
 
   }
 
-}
 
-  require_once view('home');
+
+require_once view('home');
