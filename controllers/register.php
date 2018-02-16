fb65eef740a43a338d6d4df0bb1cb52037a4cc5b
alokin95 <nikola_m95@hotmail.com>
2018-02-16 21:47:25 +0100

    Deleted comments

--- controllers/register.php
+++ controllers/register.php
@@ -1,24 +1,6 @@
 <?php
 
   require_once 'classes/User.php';
-  //require_once 'classes/Register.php';
-  
-  // if (isset($_POST['register'])){
-    
-  //   $first= $_POST['firstName'];
-  //   $last = $_POST['lastName'];
-  //   $password = md5($_POST['password']);
-  //   $mail = $_POST['email'];
-   
-  //   $register = new Register($first, $last, $password, $mail);
-
-  //   $register->registerUser();
-
-  //   //require_once 'views/register.template.php';
-
-  //   require_once view('register');
-
-  // }
 
   if (isset($_POST['register'])){
     
